a7787bacd27e1aa107c656f9667b38d3af0fd975
Lucas Huang <lucashsw@gmail.com>
2021-05-16 17:36:35 +1200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express') //common js modules
+
+//import express from 'express' //ex2015
+
+const app = express();
+
+
+app.get('/', (req,res) => {
+  res.send({hi:'there'})
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
