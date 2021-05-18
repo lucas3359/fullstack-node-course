d2d74e0a5e3d8411e0419cc2f49b967a137273d2
Lucas Huang <lucashsw@gmail.com>
2021-05-18 19:37:56 +1200

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,25 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+}
+
+
+
+
+//
+// myuser
+//
+//
+//
+// mongodb+srv://myuser:eoJwzpwoFhgGjFN3@cluster0.nlk87.mongodb.net/emaily-prod?retryWrites=true&w=majority
+//
+//
+//
+//
+//
+// 111122786589-320kqmmmdfshha50uadj7oa5o4avf026.apps.googleusercontent.com
+//
+//
+// 99VWbK62td3rQiMACuq8EA--
--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const express = require('express') //common js modules
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
 
-//import express from 'express' //ex2015
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', (req,res) => {
-  res.send({hi:'there we changed'})
-});
+require('./routes/authRoutes')(app);
 
+//by heroko
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
